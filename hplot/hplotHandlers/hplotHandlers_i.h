e103ad3f807197c8c8bbdca2f08588da3f427441
huapeng01016 <hpeng01016@yahoo.com>
2022-09-30 15:59:25 -0500

    modified:   hplot/hplotHandlers/hplotHandlers.vcxproj 	modified:   hplot/hplotHandlers/hplotHandlers_i.h

--- hplot/hplotHandlers/hplotHandlers_i.h
+++ hplot/hplotHandlers/hplotHandlers_i.h
@@ -1,1 +1,388 @@
-/* MIDL: this ALWAYS GENERATED file contains the definitions for the interfaces */
+
+
+/* this ALWAYS GENERATED file contains the definitions for the interfaces */
+
+
+ /* File created by MIDL compiler version 8.01.0626 */
+/* at Mon Jan 18 21:14:07 2038
+ */
+/* Compiler settings for hplotHandlers.idl:
+    Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 8.01.0626 
+    protocol : all , ms_ext, c_ext, robust
+    error checks: allocation ref bounds_check enum stub_data 
+    VC __declspec() decoration level: 
+         __declspec(uuid()), __declspec(selectany), __declspec(novtable)
+         DECLSPEC_UUID(), MIDL_INTERFACE()
+*/
+/* @@MIDL_FILE_HEADING(  ) */
+
+
+
+/* verify that the <rpcndr.h> version is high enough to compile this file*/
+#ifndef __REQUIRED_RPCNDR_H_VERSION__
+#define __REQUIRED_RPCNDR_H_VERSION__ 500
+#endif
+
+#include "rpc.h"
+#include "rpcndr.h"
+
+#ifndef __RPCNDR_H_VERSION__
+#error this stub requires an updated version of <rpcndr.h>
+#endif /* __RPCNDR_H_VERSION__ */
+
+#ifndef COM_NO_WINDOWS_H
+#include "windows.h"
+#include "ole2.h"
+#endif /*COM_NO_WINDOWS_H*/
+
+#ifndef __hplotHandlers_i_h__
+#define __hplotHandlers_i_h__
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1020)
+#pragma once
+#endif
+
+#ifndef DECLSPEC_XFGVIRT
+#if _CONTROL_FLOW_GUARD_XFG
+#define DECLSPEC_XFGVIRT(base, func) __declspec(xfg_virtual(base, func))
+#else
+#define DECLSPEC_XFGVIRT(base, func)
+#endif
+#endif
+
+/* Forward Declarations */ 
+
+#ifndef __IPreview_FWD_DEFINED__
+#define __IPreview_FWD_DEFINED__
+typedef interface IPreview IPreview;
+
+#endif 	/* __IPreview_FWD_DEFINED__ */
+
+
+#ifndef __IThumbnail_FWD_DEFINED__
+#define __IThumbnail_FWD_DEFINED__
+typedef interface IThumbnail IThumbnail;
+
+#endif 	/* __IThumbnail_FWD_DEFINED__ */
+
+
+#ifndef __ISearch_FWD_DEFINED__
+#define __ISearch_FWD_DEFINED__
+typedef interface ISearch ISearch;
+
+#endif 	/* __ISearch_FWD_DEFINED__ */
+
+
+#ifndef __Preview_FWD_DEFINED__
+#define __Preview_FWD_DEFINED__
+
+#ifdef __cplusplus
+typedef class Preview Preview;
+#else
+typedef struct Preview Preview;
+#endif /* __cplusplus */
+
+#endif 	/* __Preview_FWD_DEFINED__ */
+
+
+#ifndef __Thumbnail_FWD_DEFINED__
+#define __Thumbnail_FWD_DEFINED__
+
+#ifdef __cplusplus
+typedef class Thumbnail Thumbnail;
+#else
+typedef struct Thumbnail Thumbnail;
+#endif /* __cplusplus */
+
+#endif 	/* __Thumbnail_FWD_DEFINED__ */
+
+
+#ifndef __Search_FWD_DEFINED__
+#define __Search_FWD_DEFINED__
+
+#ifdef __cplusplus
+typedef class Search Search;
+#else
+typedef struct Search Search;
+#endif /* __cplusplus */
+
+#endif 	/* __Search_FWD_DEFINED__ */
+
+
+/* header files for imported files */
+#include "oaidl.h"
+#include "ocidl.h"
+#include "shobjidl.h"
+#include "thumbcache.h"
+#include "filter.h"
+
+#ifdef __cplusplus
+extern "C"{
+#endif 
+
+
+#ifndef __IPreview_INTERFACE_DEFINED__
+#define __IPreview_INTERFACE_DEFINED__
+
+/* interface IPreview */
+/* [unique][uuid][object] */ 
+
+
+EXTERN_C const IID IID_IPreview;
+
+#if defined(__cplusplus) && !defined(CINTERFACE)
+    
+    MIDL_INTERFACE("03af2809-ad88-4c6b-a1cd-470a04fcff02")
+    IPreview : public IUnknown
+    {
+    public:
+    };
+    
+    
+#else 	/* C style interface */
+
+    typedef struct IPreviewVtbl
+    {
+        BEGIN_INTERFACE
+        
+        DECLSPEC_XFGVIRT(IUnknown, QueryInterface)
+        HRESULT ( STDMETHODCALLTYPE *QueryInterface )( 
+            IPreview * This,
+            /* [in] */ REFIID riid,
+            /* [annotation][iid_is][out] */ 
+            _COM_Outptr_  void **ppvObject);
+        
+        DECLSPEC_XFGVIRT(IUnknown, AddRef)
+        ULONG ( STDMETHODCALLTYPE *AddRef )( 
+            IPreview * This);
+        
+        DECLSPEC_XFGVIRT(IUnknown, Release)
+        ULONG ( STDMETHODCALLTYPE *Release )( 
+            IPreview * This);
+        
+        END_INTERFACE
+    } IPreviewVtbl;
+
+    interface IPreview
+    {
+        CONST_VTBL struct IPreviewVtbl *lpVtbl;
+    };
+
+    
+
+#ifdef COBJMACROS
+
+
+#define IPreview_QueryInterface(This,riid,ppvObject)	\
+    ( (This)->lpVtbl -> QueryInterface(This,riid,ppvObject) ) 
+
+#define IPreview_AddRef(This)	\
+    ( (This)->lpVtbl -> AddRef(This) ) 
+
+#define IPreview_Release(This)	\
+    ( (This)->lpVtbl -> Release(This) ) 
+
+
+#endif /* COBJMACROS */
+
+
+#endif 	/* C style interface */
+
+
+
+
+#endif 	/* __IPreview_INTERFACE_DEFINED__ */
+
+
+#ifndef __IThumbnail_INTERFACE_DEFINED__
+#define __IThumbnail_INTERFACE_DEFINED__
+
+/* interface IThumbnail */
+/* [unique][uuid][object] */ 
+
+
+EXTERN_C const IID IID_IThumbnail;
+
+#if defined(__cplusplus) && !defined(CINTERFACE)
+    
+    MIDL_INTERFACE("fb258c7b-0758-41be-aa59-318f5d5fbbbd")
+    IThumbnail : public IUnknown
+    {
+    public:
+    };
+    
+    
+#else 	/* C style interface */
+
+    typedef struct IThumbnailVtbl
+    {
+        BEGIN_INTERFACE
+        
+        DECLSPEC_XFGVIRT(IUnknown, QueryInterface)
+        HRESULT ( STDMETHODCALLTYPE *QueryInterface )( 
+            IThumbnail * This,
+            /* [in] */ REFIID riid,
+            /* [annotation][iid_is][out] */ 
+            _COM_Outptr_  void **ppvObject);
+        
+        DECLSPEC_XFGVIRT(IUnknown, AddRef)
+        ULONG ( STDMETHODCALLTYPE *AddRef )( 
+            IThumbnail * This);
+        
+        DECLSPEC_XFGVIRT(IUnknown, Release)
+        ULONG ( STDMETHODCALLTYPE *Release )( 
+            IThumbnail * This);
+        
+        END_INTERFACE
+    } IThumbnailVtbl;
+
+    interface IThumbnail
+    {
+        CONST_VTBL struct IThumbnailVtbl *lpVtbl;
+    };
+
+    
+
+#ifdef COBJMACROS
+
+
+#define IThumbnail_QueryInterface(This,riid,ppvObject)	\
+    ( (This)->lpVtbl -> QueryInterface(This,riid,ppvObject) ) 
+
+#define IThumbnail_AddRef(This)	\
+    ( (This)->lpVtbl -> AddRef(This) ) 
+
+#define IThumbnail_Release(This)	\
+    ( (This)->lpVtbl -> Release(This) ) 
+
+
+#endif /* COBJMACROS */
+
+
+#endif 	/* C style interface */
+
+
+
+
+#endif 	/* __IThumbnail_INTERFACE_DEFINED__ */
+
+
+#ifndef __ISearch_INTERFACE_DEFINED__
+#define __ISearch_INTERFACE_DEFINED__
+
+/* interface ISearch */
+/* [unique][uuid][object] */ 
+
+
+EXTERN_C const IID IID_ISearch;
+
+#if defined(__cplusplus) && !defined(CINTERFACE)
+    
+    MIDL_INTERFACE("bd1349aa-9044-4c1d-a88d-aec1b8121e1c")
+    ISearch : public IUnknown
+    {
+    public:
+    };
+    
+    
+#else 	/* C style interface */
+
+    typedef struct ISearchVtbl
+    {
+        BEGIN_INTERFACE
+        
+        DECLSPEC_XFGVIRT(IUnknown, QueryInterface)
+        HRESULT ( STDMETHODCALLTYPE *QueryInterface )( 
+            ISearch * This,
+            /* [in] */ REFIID riid,
+            /* [annotation][iid_is][out] */ 
+            _COM_Outptr_  void **ppvObject);
+        
+        DECLSPEC_XFGVIRT(IUnknown, AddRef)
+        ULONG ( STDMETHODCALLTYPE *AddRef )( 
+            ISearch * This);
+        
+        DECLSPEC_XFGVIRT(IUnknown, Release)
+        ULONG ( STDMETHODCALLTYPE *Release )( 
+            ISearch * This);
+        
+        END_INTERFACE
+    } ISearchVtbl;
+
+    interface ISearch
+    {
+        CONST_VTBL struct ISearchVtbl *lpVtbl;
+    };
+
+    
+
+#ifdef COBJMACROS
+
+
+#define ISearch_QueryInterface(This,riid,ppvObject)	\
+    ( (This)->lpVtbl -> QueryInterface(This,riid,ppvObject) ) 
+
+#define ISearch_AddRef(This)	\
+    ( (This)->lpVtbl -> AddRef(This) ) 
+
+#define ISearch_Release(This)	\
+    ( (This)->lpVtbl -> Release(This) ) 
+
+
+#endif /* COBJMACROS */
+
+
+#endif 	/* C style interface */
+
+
+
+
+#endif 	/* __ISearch_INTERFACE_DEFINED__ */
+
+
+
+#ifndef __hplotHandlersLib_LIBRARY_DEFINED__
+#define __hplotHandlersLib_LIBRARY_DEFINED__
+
+/* library hplotHandlersLib */
+/* [version][uuid] */ 
+
+
+EXTERN_C const IID LIBID_hplotHandlersLib;
+
+EXTERN_C const CLSID CLSID_Preview;
+
+#ifdef __cplusplus
+
+class DECLSPEC_UUID("5a58cd61-1152-41bf-8b4e-211983b3d596")
+Preview;
+#endif
+
+EXTERN_C const CLSID CLSID_Thumbnail;
+
+#ifdef __cplusplus
+
+class DECLSPEC_UUID("08c69d66-889d-4481-ba21-86f73d0b8a51")
+Thumbnail;
+#endif
+
+EXTERN_C const CLSID CLSID_Search;
+
+#ifdef __cplusplus
+
+class DECLSPEC_UUID("808b5a26-a5ac-412b-85db-4f9133455b3c")
+Search;
+#endif
+#endif /* __hplotHandlersLib_LIBRARY_DEFINED__ */
+
+/* Additional Prototypes for ALL interfaces */
+
+/* end of Additional Prototypes */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+
